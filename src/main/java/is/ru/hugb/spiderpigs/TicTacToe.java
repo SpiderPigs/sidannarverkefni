21d3cdefb845b577c3a49cc0007add8ecb1433d8
Grímur Daníelsson <grimur.danielsson@gmail.com>
2013-11-22 15:28:37 +0000

    Can now post the board to a function and the function puts the board into an array and returns null

--- src/main/java/is/ru/hugb/spiderpigs/TicTacToe.java
+++ src/main/java/is/ru/hugb/spiderpigs/TicTacToe.java
@@ -24,10 +24,22 @@ public class TicTacToe {
         post(new Route("/play") {
             @Override
             public Object handle(Request request, Response response) {
-                Integer a = Integer.valueOf(request.queryParams("a"));
-                Integer b = Integer.valueOf(request.queryParams("b"));
-                String O = String.valueOf(request.queryParams("x0y0"));
-                return O;
+                // Create array that looks like game board
+                // We then get the board from the request and put it
+                // into the array.
+                String[][] ticTacToeBoard = new String[3][3];
+                ticTacToeBoard[0][0] = request.queryParams("x0y0");
+                ticTacToeBoard[1][0] = request.queryParams("x1y0");
+                ticTacToeBoard[2][0] = request.queryParams("x2y0");
+                ticTacToeBoard[0][1] = request.queryParams("x0y1");
+                ticTacToeBoard[1][1] = request.queryParams("x1y1");
+                ticTacToeBoard[2][1] = request.queryParams("x2y1");
+                ticTacToeBoard[0][2] = request.queryParams("x0y2");
+                ticTacToeBoard[1][2] = request.queryParams("x1y2");
+                ticTacToeBoard[2][2] = request.queryParams("x2y2");
+
+
+                return null;
             }
         });
 
