ca4cc72522516723374b3b4447e922d31b11d076
atlig12 <atlig12@ru.is>
2013-11-22 13:14:24 +0000

    Initial tests for TicTacToeTest.java

--- src/test/java/is/ru/hugb/spiderpigs/TicTacToeTest.java
+++ src/test/java/is/ru/hugb/spiderpigs/TicTacToeTest.java
@@ -1,5 +1,4 @@
 package is.ru.hugb.spiderpigs;
-
 /**
  * Created with IntelliJ IDEA.
  * User: Grimur
@@ -7,5 +6,22 @@ package is.ru.hugb.spiderpigs;
  * Time: 12:12
  * To change this template use File | Settings | File Templates.
  */
+import org.junit.Test;
+
+import static org.junit.framework.Assert.assertEquals;
+import static org.junit.framework.Assert.*;
+
 public class TicTacToeTest {
+	
+	private Board board;
+	
+	@Test
+	public void isBoardEmpty(){
+		// Board empty when created
+		assertEquals(true, this.board.isEmpty());
+	}
+	@Test
+	public void boardIsNotEmpty(){
+		assertEquals(false, this.board.isFull());
+	}
 }
