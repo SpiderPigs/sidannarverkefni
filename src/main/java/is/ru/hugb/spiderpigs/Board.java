34f91cb82e0e84dc5f35c590d2321e3b75b72f4b
Grímur Daníelsson <grimur.danielsson@gmail.com>
2013-11-23 11:50:51 +0000

    Now adding game to separate class

---
+++ src/main/java/is/ru/hugb/spiderpigs/Board.java
@@ -0,0 +1,62 @@
+package is.ru.hugb.spiderpigs;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Grimur
+ * Date: 23.11.2013
+ * Time: 11:45
+ * To change this template use File | Settings | File Templates.
+ */
+public class Board {
+    private String[][] ticTacToeBoard;
+
+    public Board(){
+        ticTacToeBoard = new String[3][3];
+        for(int i = 0; i < 3; i++)
+            for(int k = 0; k < 3; k++)
+                ticTacToeBoard[i][k] = "0";
+    }
+
+    public Board(String[][] newBoard) {
+        ticTacToeBoard = newBoard;
+    }
+
+    public boolean isEmpty() {
+        return true;
+    }
+
+    public boolean isFull() {
+        return true;
+    }
+
+    public boolean someoneWinner() {
+
+        // Vertical
+        for(int i = 0; i < 3; i++)
+        {
+            if( (ticTacToeBoard[i][0] == ticTacToeBoard[i][1]) && (ticTacToeBoard[i][1] == ticTacToeBoard[i][2]) && (ticTacToeBoard[i][0] != "0") )
+            {
+                return true;
+            }
+        }
+
+        // Horizontal
+        for(int i = 0; i < 3; i++)
+        {
+            if( (ticTacToeBoard[0][i] == ticTacToeBoard[1][i]) && (ticTacToeBoard[1][i] == ticTacToeBoard[2][i]) && (ticTacToeBoard[0][i] != "0") )
+            {
+                return true;
+            }
+        }
+
+        // Diagonal - Down
+        if( (ticTacToeBoard[0][0] == ticTacToeBoard[1][1]) && (ticTacToeBoard[1][1] == ticTacToeBoard[2][2]) && (ticTacToeBoard[0][0] != "0") )
+            return true;
+
+        // Diagonal - Up
+        if( (ticTacToeBoard[0][2] == ticTacToeBoard[1][1]) && (ticTacToeBoard[1][1] == ticTacToeBoard[2][0]) && (ticTacToeBoard[0][2] != "0") )
+            return true;
+
+        return false;
+    }
+}
